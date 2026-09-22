dc0e5294da5b5b2170e776ee8c9fab1e864d1e4e
kargatiyamayur92 <kargatiyamayur92@gmail.com>
2026-09-22 21:10:08 +0530

    change port in transpoter

--- server/controllers/Auth.controllers.js
+++ server/controllers/Auth.controllers.js
@@ -142,7 +142,9 @@ export const logiOTPSEND = async (req, res) => {
 
         const transpoter = nodemailer.createTransport(
             {
-                service: "gmail",
+                host: "smtp.gmail.com",
+                port: 587,
+                secure: false,
                 auth: {
                     user: process.env.EMAIL,
                     pass: process.env.EMAIL_PASSWORD
