49fbcf9e97187e34f7cc21ab7a84ae95aca73634
kargatiyamayur92 <kargatiyamayur92@gmail.com>
2026-09-22 20:05:46 +0530

    one router denite for all users

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -4,54 +4,60 @@ import fs from 'fs'
 
 export const profileupdate = async (req, res) => {
     try {
-        const { name, id } = req.params;
-
-        const user = await usermodel.findOne({ _id: id });
-
-        if (!user) {
-            return res.json({
-                success: false,
-                msg: "User does not exist"
-            });
-        }
-
-        // Save old image before replacing it
-        const oldProfileImage = user.profileimage;
-
-        // New uploaded image
-        const newProfileImage = req.file?.filename;
-
-        if (!newProfileImage) {
-            return res.json({
-                success: false,
-                msg: "Profile image is required"
-            });
-        }
-
-        // Update user
-        user.profileimage = newProfileImage;
-        user.lastName = name;
-        user.firstName = "";
-
-        await user.save();
-
-        // Delete old image
-        if (oldProfileImage) {
-            const oldImagePath = `public/images/${oldProfileImage}`;
-
-            if (fs.existsSync(oldImagePath)) {
-                fs.unlink(oldImagePath, (err) => {
-                    if (err) {
-                        console.error("Old profile image delete error:", err);
-                    }
-                });
+        return res.json(
+            {
+                success:false,
+                msg:"This router currently unaviliable"
             }
-        }
+        )
+        // const { name, id } = req.params;
 
-        return res.json({
-            success: true,
-            msg: "User profile successfully updated"
-        });
+        // const user = await usermodel.findOne({ _id: id });
+
+        // if (!user) {
+        //     return res.json({
+        //         success: false,
+        //         msg: "User does not exist"
+        //     });
+        // }
+
+        // // Save old image before replacing it
+        // const oldProfileImage = user.profileimage;
+
+        // // New uploaded image
+        // const newProfileImage = req.file?.filename;
+
+        // if (!newProfileImage) {
+        //     return res.json({
+        //         success: false,
+        //         msg: "Profile image is required"
+        //     });
+        // }
+
+        // // Update user
+        // user.profileimage = newProfileImage;
+        // user.lastName = name;
+        // user.firstName = "";
+
+        // await user.save();
+
+        // // Delete old image
+        // if (oldProfileImage) {
+        //     const oldImagePath = `public/images/${oldProfileImage}`;
+
+        //     if (fs.existsSync(oldImagePath)) {
+        //         fs.unlink(oldImagePath, (err) => {
+        //             if (err) {
+        //                 console.error("Old profile image delete error:", err);
+        //             }
+        //         });
+        //     }
+        // }
+
+        // return res.json({
+        //     success: true,
+        //     msg: "User profile successfully updated"
+        // });
 
     } catch (error) {
         console.error(error);
