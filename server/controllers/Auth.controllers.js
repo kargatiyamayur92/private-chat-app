6a4add233a5b6025772c00018eaef1968d4b32f8
kargatiyamayur92 <kargatiyamayur92@gmail.com>
2026-09-25 20:29:44 +0530

    fix again name

--- server/controllers/Auth.controllers.js
+++ server/controllers/Auth.controllers.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import nodemailer from 'nodemailer'
 import OTPGenerator from './OTPGenerator.js'
-import { sendOTPEmail } from './emailSender.js' 
+import { sendOTPEmail } from './emailSender.js'
 
 export const register = async (req, res) => {
     try {
